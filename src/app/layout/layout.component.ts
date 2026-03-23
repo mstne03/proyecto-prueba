35e0369aa3db935e57166653c2ec69b3ee422059
mstne03 <mserraitorne@gmail.com>
2026-03-23 22:54:41 +0100

    feat: update layout component

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon } from '@ionic/angular/standalone';
+import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { storefrontOutline, searchOutline, bookmarkOutline, personOutline } from 'ionicons/icons';
+import { homeOutline, searchOutline, bookmarkOutline, personOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-layout',
   templateUrl: 'layout.component.html',
   styleUrls: ['layout.component.scss'],
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon],
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
 export class LayoutComponent {
   constructor() {
-    addIcons({ storefrontOutline, searchOutline, bookmarkOutline, personOutline });
+    addIcons({ homeOutline, searchOutline, bookmarkOutline, personOutline });
   }
 }
